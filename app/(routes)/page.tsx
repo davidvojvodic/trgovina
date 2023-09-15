b6766f436d910e7d6f4d7eebd61d4bdeaebd78ac
David Vojvodić <davidvojvodic1@gmail.com>
2023-09-15 14:48:19 +0200

    first commit

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,17 @@
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Container>
+        <div className="space-y-10 pb-10">
+          <Billboard />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default HomePage;
