e080a49134fc54da7af7ed3c7019caf538186b0a
David  Vojvodić <davidvojvodic1@gmail.com>
2024-01-13 19:10:15 +0100

    fix2

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -1,119 +1,119 @@
-"use client";
+// "use client";
 
-import Container from "@/components/ui/container";
-import * as z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { useRouter } from "next/navigation";
-import { sendEmail } from "@/actions/send-email";
-import toast from "react-hot-toast";
+// import Container from "@/components/ui/container";
+// import * as z from "zod";
+// import { useForm } from "react-hook-form";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { Button } from "@/components/ui/button";
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { Input } from "@/components/ui/input";
+// import { Textarea } from "@/components/ui/textarea";
+// import { useRouter } from "next/navigation";
+// import { sendEmail } from "@/actions/send-email";
+// import toast from "react-hot-toast";
 
-export const formSchema = z.object({
-  name: z.string().min(1, { message: "Name is required" }),
-  email: z
-    .string()
-    .min(1, { message: "Email is required" })
-    .email({ message: "Email is invalid" }),
-  message: z.string().min(1, { message: "Message is required" }),
-});
+// export const formSchema = z.object({
+//   name: z.string().min(1, { message: "Name is required" }),
+//   email: z
+//     .string()
+//     .min(1, { message: "Email is required" })
+//     .email({ message: "Email is invalid" }),
+//   message: z.string().min(1, { message: "Message is required" }),
+// });
 
-const ContactPage = () => {
-  const router = useRouter();
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      email: "",
-      message: "",
-    },
-  });
+// const ContactPage = () => {
+//   const router = useRouter();
+//   const form = useForm<z.infer<typeof formSchema>>({
+//     resolver: zodResolver(formSchema),
+//     defaultValues: {
+//       name: "",
+//       email: "",
+//       message: "",
+//     },
+//   });
 
-  const onSubmit = async (data: z.infer<typeof formSchema>) => {
-    try {
-      await sendEmail(data);
+//   const onSubmit = async (data: z.infer<typeof formSchema>) => {
+//     try {
+//       await sendEmail(data);
 
-      toast.success("Sent successfully!");
+//       toast.success("Sent successfully!");
 
-      form.reset();
-    } catch (error) {
-      toast.error("An error occured");
-      console.log(error);
-    }
-  };
-  return (
-    <div className="bg-white">
-      <Container>
-        <div className="px-4 py-10 sm:px-6 lg:px-8">
-          <div className="px-20 py-10 border-2 rounded-lg shadow-lg mx-10">
-            <Form {...form}>
-              <form
-                className="space-y-8"
-                onSubmit={form.handleSubmit(onSubmit)}
-              >
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Name</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Name" {...field} />
-                      </FormControl>
+//       form.reset();
+//     } catch (error) {
+//       toast.error("An error occured");
+//       console.log(error);
+//     }
+//   };
+//   return (
+//     <div className="bg-white">
+//       <Container>
+//         <div className="px-4 py-10 sm:px-6 lg:px-8">
+//           <div className="px-20 py-10 border-2 rounded-lg shadow-lg mx-10">
+//             <Form {...form}>
+//               <form
+//                 className="space-y-8"
+//                 onSubmit={form.handleSubmit(onSubmit)}
+//               >
+//                 <FormField
+//                   control={form.control}
+//                   name="name"
+//                   render={({ field }) => (
+//                     <FormItem>
+//                       <FormLabel>Name</FormLabel>
+//                       <FormControl>
+//                         <Input placeholder="Name" {...field} />
+//                       </FormControl>
 
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Email" {...field} />
-                      </FormControl>
+//                       <FormMessage />
+//                     </FormItem>
+//                   )}
+//                 />
+//                 <FormField
+//                   control={form.control}
+//                   name="email"
+//                   render={({ field }) => (
+//                     <FormItem>
+//                       <FormLabel>Email</FormLabel>
+//                       <FormControl>
+//                         <Input placeholder="Email" {...field} />
+//                       </FormControl>
 
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="message"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Message</FormLabel>
-                      <FormControl>
-                        <Textarea rows={10} placeholder="Message" {...field} />
-                      </FormControl>
+//                       <FormMessage />
+//                     </FormItem>
+//                   )}
+//                 />
+//                 <FormField
+//                   control={form.control}
+//                   name="message"
+//                   render={({ field }) => (
+//                     <FormItem>
+//                       <FormLabel>Message</FormLabel>
+//                       <FormControl>
+//                         <Textarea rows={10} placeholder="Message" {...field} />
+//                       </FormControl>
 
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button className="w-full font-bold text-md text" type="submit">
-                  Submit
-                </Button>
-              </form>
-            </Form>
-          </div>
-        </div>
-      </Container>
-    </div>
-  );
-};
+//                       <FormMessage />
+//                     </FormItem>
+//                   )}
+//                 />
+//                 <Button className="w-full font-bold text-md text" type="submit">
+//                   Submit
+//                 </Button>
+//               </form>
+//             </Form>
+//           </div>
+//         </div>
+//       </Container>
+//     </div>
+//   );
+// };
 
-export default ContactPage;
+// export default ContactPage;
