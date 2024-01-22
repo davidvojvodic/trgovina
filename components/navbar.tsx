435dbe100e60b4ac091e01d996d6e5bd7c2b8bbd
David  Vojvodić <davidvojvodic1@gmail.com>
2024-01-22 19:48:13 +0100

    fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,11 +14,11 @@ const Navbar = async () => {
   const store = await getStore();
 
   return (
-    <div className="border-b py-9">
+    <div className="border-b py-5">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 justify-between flex items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <Image src={`${store.storeImage}`} alt="Logo" width={150} height={50} />
+            <Image src={`${store.storeImage}`} alt="Logo" width={50} height={50} />
           </Link>
           <MainNav data={categories} /> 
           <NavbarActions />
